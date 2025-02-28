b966707a6a7dcb4c79f9cd477a5595272ed374de
GregorSchulze <gr.trash@t-online.de>
2025-02-28 13:46:42 +0100

    Update

---
+++ src/App.jsx
@@ -0,0 +1,55 @@
+import { useState, useEffect } from "react";
+import "./styles.css";
+import { NewTodoForm } from "./NewTodoForm";
+import { TodoList } from "./TodoList";
+
+export default function App() {
+  const [todos, setTodos] = useState(() => {
+    const localValue = localStorage.getItem("ITEMS"); // Überprüft den LocalStorage. Existiert ein Wert, wird deser gespeichert
+    if (localValue == null) return [];
+    return JSON.parse(localValue);
+  });
+
+  // Local Storage
+  useEffect(() => {
+    localStorage.setItem("ITEMS", JSON.stringify(todos));
+  }, [todos]);
+
+  // New ToDO
+  function addTodo(title) {
+    setTodos((currentTodos) => {
+      return [
+        ...currentTodos,
+        { id: crypto.randomUUID(), title, completed: false },
+      ];
+    });
+  }
+
+  // Toggle
+  function toggleToDo(id, completed) {
+    setTodos((currentTodos) => {
+      return currentTodos.map((todo) => {
+        if (todo.id === id) {
+          return { ...todo, completed };
+        }
+
+        return todo;
+      });
+    });
+  }
+
+  // Delete
+  function deleteTodo(id) {
+    setTodos((currentTodos) => {
+      return currentTodos.filter((todo) => todo.id !== id);
+    });
+  }
+
+  return (
+    <>
+      <NewTodoForm onSubmit={addTodo} />
+      <h1 className="header">ToDo List</h1>
+      <TodoList todos={todos} toggleToDo={toggleToDo} deleteTodo={deleteTodo} />
+    </>
+  );
+}
